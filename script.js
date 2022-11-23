2d55703c13c39f3130931ce0cf2d5cbfe55f5c10
Mayeul DERIES <mayeul.deries@etu.univ-grenoble-alpes.fr>
2022-11-23 19:15:37 +0100

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+var slide = new Array("img/cloud3.jpeg", "img/fibre2.png", "img/environnement.jpg");
+var numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
+
+setInterval("ChangeSlide(1)", 4000);
